08e17ecd5c2aeacbd3bd513013ae09dfa71994c2
MD5HashBrowns <md5hashbrownz@gmail.com>
2017-10-22 21:49:24 -0400

    Removed test pages for time being, using standardjs and ejs-linter for tests

--- test/test-pages.js
+++
@@ -1,9 +0,0 @@
-var expect = require('chai').expect
-var request = require('request')
-
-it('Main page content', function (done) {
-  request('http://localhost:3000', function (error, response, body) {
-    expect(response.statusCode).to.equal(200)
-    done()
-  })
-})

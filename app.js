2a56c893928d3c12ee651996dfd33562ae533807
MD5HashBrowns <md5hashbrownz@gmail.com>
2017-10-22 21:23:57 -0400

    Fixed travis, made code follow standard code style

--- app.js
+++ app.js
@@ -3,17 +3,16 @@ const bodyParser = require('body-parser')
 const path = require('path')
 const request = require('request')
 const urlencode = require('urlencode')
-const iframeReplacement = require('node-iframe-replacement');
-
+const iframeReplacement = require('node-iframe-replacement')
 
 // Pull in config enviroment variables
 require('env2')('./config.env')
 
 // Handle some errors if default values
-if (process.env.ALLUC_API_KEY == 'INSERT_ALLUC_API_KEY_HERE') {
+if (process.env.ALLUC_API_KEY === 'INSERT_ALLUC_API_KEY_HERE') {
   throw Error('You have not entered your Alluc API Key in config.env. Please visit http://accounts.alluc.com/ and register for an account to get an API key.')
 }
-if (process.env.TMDB_API_KEY == 'INSERT_TMDB_API_KEY_HERE') {
+if (process.env.TMDB_API_KEY === 'INSERT_TMDB_API_KEY_HERE') {
   throw Error('You have not entered your Trakt ID and/or Trakt Secret in config.env. Please visit https://trakt.tv/oauth/applications/new to create a Trakt app and get these credientials')
 }
 
@@ -22,28 +21,26 @@ const tmdb = require('moviedb')(process.env.TMDB_API_KEY)
 const app = express()
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
-app.use(express.static(__dirname + '/views'))
+app.use(express.static(path.join(__dirname, '/views')))
 
 // Body Parser middleware
-app.use(urlencodedParser = bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // iframe replacement middleware (adds res.merge)
-app.use(iframeReplacement);
+app.use(iframeReplacement)
 
-app.get('/watch-thevideo/:id', function(req, res){
-    console.log(req.params.id);
+app.get('/watch-thevideo/:id', function (req, res) {
+  console.log(req.params.id)
     // respond to this request with our fake-news content embedded within the BBC News home page
-    res.merge('nothingness', {
+  res.merge('nothingness', {
         // external url to fetch
-       sourceUrl: 'http://thevideo.me/embed-' + req.params.id + '-640x360.html',
+    sourceUrl: 'http://thevideo.me/embed-' + req.params.id + '-640x360.html',
        // css selector to inject our content into
-       sourcePlaceholder: 'script:nth-of-type(4)',
+    sourcePlaceholder: 'script:nth-of-type(4)',
        // pass a function here to intercept the source html prior to merging
-       transform: function($, model) { }
-    });
-
-});
-
+    transform: function ($, model) { }
+  })
+})
 
 app.get('/', function (req, res) {
   res.redirect('/tv-shows')
@@ -51,6 +48,9 @@ app.get('/', function (req, res) {
 
 // TMDb
 tmdb.miscPopularMovies((err, movies) => {
+  if (err) {
+    console.log("TMDB couldn't get Popular Movies")
+  }
   // Get movie page
   app.get('/movies', function (req, res) {
     res.render('movies', {
@@ -62,6 +62,9 @@ tmdb.miscPopularMovies((err, movies) => {
 })
 
 tmdb.miscPopularTvs((err, tvShows) => {
+  if (err) {
+    console.log("TMDB couldn't get Popular TV Shows")
+  }
   // Get TV Shows page
   app.get('/tv-shows', function (req, res) {
     res.render('tvShows', {
@@ -72,27 +75,27 @@ tmdb.miscPopularTvs((err, tvShows) => {
   })
 })
 
-
 // NOTE: Instead of having two watch gets I could have one and check whether it's a movie or not based on it's ID, they could render either watchMovie or watchTvShow. But I mean, whos got time for that.
 app.get('/watch-tv-show/:id', function (req, res) {
-  tmdb.tvInfo({ id: req.params.id}, (err, tvInfo) => {
-
-
-      res.render('watchTvShow', { // Render the watch page and pass some variables
-        title: 'Dionysus',
-        tvInfo: tvInfo,
-        page: 'tvShows'
-      })
+  tmdb.tvInfo({id: req.params.id}, (err, tvInfo) => {
+    if (err) {
+      console.log("TMDB Couldn't retrieve TV Show info")
+    }
+    res.render('watchTvShow', { // Render the watch page and pass some variables
+      title: 'Dionysus',
+      tvInfo: tvInfo,
+      page: 'tvShows'
     })
   })
+})
 
-
-app.get('/watch-episode/:id', function (req, res){
-
-
+app.get('/watch-episode/:id', function (req, res) {
   // I must directly interact with Alluc API via `request` due to lack of npm module
   // Concatenation is fun :D
   request('https://www.alluc.ee/api/search/stream/?apikey=' + process.env.ALLUC_API_KEY + '&query=' + urlencode(req.params.id) + '%20' + process.env.QUALITY + '%20' + 'host%3Aopenload.co' + '&count=4&from=0&getmeta=0', function (error, response, body) {
+    if (error) {
+      console.log('Alluc API request for episode failed')
+    }
     // Simple JSON parse from the request
 
     var parsedBody = JSON.parse(body)
@@ -109,7 +112,7 @@ app.get('/watch-episode/:id', function (req, res){
       var streamsError = '' // Sets the stream error content to nothing
     } else {
       console.log("ERR: There's no streams available") // Logging
-      var streamsError = 'Sorry, there are no streams available for this TV Show :(' // Gives stream error some content
+      streamsError = 'Sorry, there are no streams available for this TV Show :(' // Gives stream error some content
     }
 
     console.log('LINKS: ' + links) // Logging
@@ -120,16 +123,21 @@ app.get('/watch-episode/:id', function (req, res){
       link4: links[3],
       title: 'Dionysus',
       page: 'tvShows',
-      streamsError, streamsError
+      streamsError
     })
   })
 })
 
 app.get('/watch-movie/:id', function (req, res) {
-
-  tmdb.movieInfo({ id: req.params.id}, (err, movieInfo) => {
+  tmdb.movieInfo({id: req.params.id}, (err, movieInfo) => {
+    if (err) {
+      console.log("TMDB Couldn't retrieve movie info")
+    }
     // Concatenation is fun :D
     request('https://www.alluc.ee/api/search/stream/?apikey=' + process.env.ALLUC_API_KEY + '&query=' + movieInfo.title + '%20' + movieInfo.release_date.substring(0, 4) + '%20' + 'host%3Athevideo.me' + '&count=4&from=0&getmeta=1', function (error, response, body) {
+      if (error) {
+        console.log('Alluc API request for movie failed')
+      }
       // Simple JSON parse from the request
       var parsedBody = JSON.parse(body)
 
@@ -139,14 +147,13 @@ app.get('/watch-movie/:id', function (req, res) {
       }
       if (parsedBody['result'].length > 0) {
         for (let i = 0; i < parsedBody['result'].length; i++) {
-
           links[i] = parsedBody['result'][i]['hosterurls'][0]['url'] // Replaces "" with links
           console.log(parsedBody['result'][i]['hosterurls'])
         }
         var streamsError = '' // Sets the stream error content to nothing
       } else {
         console.log("ERR: There's no streams available") // Logging
-        var streamsError = 'Sorry, there are no streams available for this TV Show / Movie :(' // Gives stream error some content
+        streamsError = 'Sorry, there are no streams available for this TV Show / Movie :(' // Gives stream error some content
       }
 
       console.log('LINKS: ' + links) // Logging
@@ -166,8 +173,11 @@ app.get('/watch-movie/:id', function (req, res) {
 })
 
 app.get('/search-movies/:id', function (req, res) {
-  search = req.params.id
+  var search = req.params.id
   tmdb.searchMovie({ query: search }, (err, response) => {
+    if (err) {
+      console.log("TMDB Couldn't search for movies")
+    }
     res.render('searchMovies', {
       title: search,
       searchResults: response.results,
@@ -181,8 +191,11 @@ app.post('/search-movies/submit', function (req, res) {
 })
 
 app.get('/search-tv-shows/:id', function (req, res) {
-  search = req.params.id
+  var search = req.params.id
   tmdb.searchTv({ query: search }, (err, response) => {
+    if (err) {
+      console.log("TMDB Couldn't search for TV Shows")
+    }
     res.render('searchTvShows', {
       title: search,
       searchResults: response.results,

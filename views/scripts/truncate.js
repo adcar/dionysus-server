52bc6ba3ec83e7052abb79f4ebdea1a34c3acf46
MD5HashBrowns <md5hashbrownz@gmail.com>
2017-11-11 12:39:30 -0500

    Changed sources to display actual title, rather than tooltip

--- views/scripts/truncate.js
+++ views/scripts/truncate.js
@@ -1,8 +1,8 @@
 window.addEventListener('load', function () {
   // Truncate function
-  const truncate = (string) => {
-    if (string.length > 15) {
-      return string.substring(0, 14) + '...'
+  const truncate = (string, length) => {
+    if (string.length > length) {
+      return string.substring(0, length - 1) + '...'
     } else {
       return string
     }
@@ -10,10 +10,19 @@ window.addEventListener('load', function () {
 
   var titles = document.querySelectorAll('.card-title')
   for (let i = 0; i < titles.length; i++) {
-    // This sets the title HTML attribute, so you can hover over and see it. (Plus accessibility)
+    // This sets the title HTML attribute, so you can hover over and see it. (Plus accessibility) Not to be confused with the card "title". (Which isn't a HTML title attribute... yeah... title)
     titles[i].setAttribute('title', titles[i].innerHTML)
 
     // This actually truncates using the function above.
-    titles[i].innerHTML = truncate(titles[i].innerHTML)
+    titles[i].innerHTML = truncate(titles[i].innerHTML, 15)
+  }
+
+  var links = document.querySelectorAll('.source')
+  for (let i = 0; i < links.length; i++) {
+    // This sets the title HTML attribute, so you can hover over and see it. (Plus accessibility)
+    links[i].setAttribute('title', links[i].innerHTML)
+
+    // This actually truncates using the function above.
+    links[i].innerHTML = truncate(links[i].innerHTML, 37)
   }
 })
